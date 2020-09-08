db9df1350cd2adb0706931b3ade7b580f9b0120c
Masiu <ciszekmarcell@gmail.com>
2020-09-08 19:53:41 +0200

    some more composition

--- composition/index.js
+++ composition/index.js
@@ -1,14 +1,14 @@
 // Composition
 
-const f = x => x + 5;
+const f = (x) => x + 5;
 
-const h = x => x * 2;
+const h = (x) => x * 2;
 
 // console.log(f(h(5)));
 
-const shout = str => str.toUpperCase();
-const shoutLouder = str => `${str}!!!!!!`;
-const repeat = str => `${str} ${str} ${str}`;
+const shout = (str) => str.toUpperCase();
+const shoutLouder = (str) => `${str}!!!!!!`;
+const repeat = (str) => `${str} ${str} ${str}`;
 
 //  runs from inside out
 // 1. shout
@@ -18,7 +18,7 @@ const repeat = str => `${str} ${str} ${str}`;
 
 // better approach to make your code cleaner and to have a compose function , similar how it works in rambda or lodash
 // x = initial value
-const compose = (...fns) => x => fns.reduceRight((acc, fn) => fn(acc), x);
+const compose = (...fns) => (x) => fns.reduceRight((acc, fn) => fn(acc), x);
 
 const iLoveToScream = compose(repeat, shoutLouder, repeat);
 
@@ -26,8 +26,19 @@ const iLoveToScream = compose(repeat, shoutLouder, repeat);
 
 // we can also use the pipe operation in javascript
 // x = initial value
-const pipe = (...fns) => x => fns.reduce((acc, fn) => fn(acc), x);
+const pipe = (...fns) => (x) => fns.reduce((acc, fn) => fn(acc), x);
 
 const iLoveToScream2 = pipe(repeat, shoutLouder, shout);
 
-console.log(iLoveToScream2('hello'));
+// console.log(iLoveToScream2('hello'));
+
+let reversed = (str) => str.split("").reverse().join("");
+let first10OfStr = (str) => str.substr(0, 10);
+
+let composedValue = compose(
+  reversed,
+  shoutLouder,
+  first10OfStr,
+)("Legia Warsazawa CWKS");
+
+console.log(composedValue);
