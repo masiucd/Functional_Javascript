b45f25f1acdec2fed8aa28a6cdefb8ab9feb625a
Masiu <ciszekmarcell@gmail.com>
2020-09-12 13:06:28 +0200

    clee game

---
+++ basic_data_structures/clue.js
@@ -0,0 +1,22 @@
+const game = {};
+
+game.murderer = "??";
+game["weapons"] = ["stick", "hammer", "knife", "pistol"];
+game.characters = [
+  { name: "Tina", age: 21, gender: 1 },
+  { name: "Logan", age: 23, gender: 0 },
+  { name: "Mia", age: 42, gender: 1 },
+];
+
+// console.log(game);
+
+function listAllChars(obj) {
+  return obj.characters.map(
+    (char) =>
+      `${char.name} is ${char.age} years old and ${
+        char.gender === 1 ? "she is" : "he is"
+      } ${char.gender === 0 ? "male" : "female"}`,
+  );
+}
+
+console.log(listAllChars(game));
