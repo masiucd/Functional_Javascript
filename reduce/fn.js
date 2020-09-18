e14bf568566fee91f80a7eba02ad67473875da74
Masiu <ciszekmarcell@gmail.com>
2020-09-18 20:19:11 +0200

    some more fn

---
+++ reduce/fn.js
@@ -0,0 +1,25 @@
+/**
+ *
+ * @param {Array} list
+ * @param {Function} fn
+ * @param {Any} initial
+ */
+const reducer = (list, fn, initial = 0) => {
+  let memo = initial;
+
+  for (let i = 0; i < list.length; i++) {
+    memo = fn(list[i], memo);
+  }
+  return memo;
+};
+
+let x = reducer([1, 2, 3], (prev, res) => prev + res, 0);
+console.log(x);
+
+// function forEachRight(arr, fn) {
+//   for (let i = arr.length; i <= 0; i--) {
+//     fn(arr[i]);
+//   }
+// }
+
+// console.log(forEachRight([1, 2, 3, 4, 5, 6], (x) => x + 1));
