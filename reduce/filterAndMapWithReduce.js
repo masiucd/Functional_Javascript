97ef096814a90d0fda2a5eefaf90d544be7f4801
Masiu <ciszekmarcell@gmail.com>
2020-10-07 19:35:12 +0200

    filter numners with reduce

--- reduce/filterAndMapWithReduce.js
+++ reduce/filterAndMapWithReduce.js
@@ -1,10 +1,10 @@
 // @ts-nocheck
-const data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 // to transform the array we basically can use map
-const double = n => n * 2;
+const double = n => n * 2
 
-const doubleNums = data.map(double);
+const doubleNums = data.map(double)
 // console.log(doubleNums);
 // [
 //   2,  4,  6,  8, 10,
@@ -12,36 +12,67 @@ const doubleNums = data.map(double);
 // ]
 
 // to only get the even number we can use filter
-const even = n => n % 2 === 0;
+const even = n => n % 2 === 0
 
-const evenNums = data.filter(even);
+const evenNums = data.filter(even)
 // console.log(evenNums);
 // [ 2, 4, 6, 8 ]
 
 // we can even chain both map and filter together
 // check out the difference of time using this implantation vs reduce
-console.time('mappedList');
-const mappedList = data.filter(n => n > 5).map(n => n ** 2);
-console.timeEnd('mappedList');
+// console.time('mappedList');
+const mappedList = data.filter(n => n > 5).map(n => n ** 2)
+// console.timeEnd('mappedList');
 
-console.time('mappedListWithReduce');
+// console.time('mappedListWithReduce');
 const mappedList2 = data.reduce((acc, val) => {
   if (val > 5) {
-    acc.push(val ** 2);
+    acc.push(val ** 2)
   }
-  return acc;
-}, []);
-console.timeEnd('mappedListWithReduce');
+  return acc
+}, [])
+// console.timeEnd('mappedListWithReduce');
 
 // mappedList: 0.102ms
 // mappedListWithReduce: 0.03ms
 
-const fruits = ['apple', 'kiwi', 'banana', 'berries'];
+const fruits = ["apple", "kiwi", "banana", "berries"]
 const filterWithReduce = fruits.reduce((basket, fruit) => {
-  if (fruit.charAt(0) === 'b') {
-    basket.push(fruit);
+  if (fruit.charAt(0) === "b") {
+    basket.push(fruit)
   }
-  return basket;
-}, []);
+  return basket
+}, [])
 
 // console.log(filterWithReduce);
+
+const nums = [
+  1,
+  2,
+  3,
+  4,
+  2,
+  2,
+  2,
+  2,
+  2,
+  5,
+  6,
+  2,
+  2,
+  2,
+  2,
+  7,
+  1,
+  1,
+  1,
+  1,
+  8,
+  9,
+  10,
+]
+
+const filterNumber = nums.reduce((prev, current) => {
+  return !prev.includes(current) ? [...prev, current] : prev
+}, [])
+console.log(filterNumber)
