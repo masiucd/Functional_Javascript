8775b9be775d39ce5cb8d798c4ddb49e95c4c07b
Masiu <ciszekmarcell@gmail.com>
2020-10-09 19:39:09 +0200

    reverse recursion

--- recursion/index.ts
+++ recursion/index.ts
@@ -2,55 +2,58 @@
 
 function counterWithForLoop(n: number): void {
   for (let i = 0; i <= n; i++) {
-    console.log(i);
+    console.log(i)
   }
 }
 
 function count(n: number): number {
-  console.log(n);
-  if (n === 10) return n;
-  return count(n + 1);
+  console.log(n)
+  if (n === 10) return n
+  return count(n + 1)
 }
 
 // console.log(count(0));
 const numbers: number[][] = [
   [1, 2, 3],
   [4, 5, 6],
-];
+]
 const number2: Array<any> = [
   [1, 2, 3],
   [4, 5, [6]],
-];
+]
 
 function findNum(items: Array<any>, n: number): string {
-  let foundIt = 'No!!!!';
+  let foundIt = "No!!!!"
   items.forEach((x, i) => {
     if (x === n) {
-      foundIt = `Yes found ${n} at index ${i}`;
+      foundIt = `Yes found ${n} at index ${i}`
     }
     if (Array.isArray(x)) {
-      foundIt = findNum(x, n);
+      foundIt = findNum(x, n)
     }
-  });
-  return foundIt;
+  })
+  return foundIt
 }
 
 // console.log(findNum(numbers, 6));
 // console.log(findNum(number2, 6));
 
 function sumArray(arr: number[]): number {
-  let res = 0;
+  let res = 0
   for (let i = 0; i < arr.length; i++) {
-    res += arr[i];
+    res += arr[i]
   }
-  return res;
+  return res
 }
 
 function sumArrayRec(arr: number[]): number {
-  if (arr.length === 0) return 0;
-  return arr[0] + sumArrayRec(arr.slice(1));
+  if (arr.length === 0) return 0
+  return arr[0] + sumArrayRec(arr.slice(1))
 }
-// console.log(sumArrayRec([1, 2, 3, 4, 5]));
+
+const reverseString = (s: string): string => {
+  return s.length === 0 ? "" : reverseString(s.slice(1)) + s[0]
+}
 
 export {
   findNum,
@@ -60,4 +63,5 @@ export {
   numbers,
   sumArray,
   sumArrayRec,
-};
+  reverseString,
+}
