4d84fa268bf7b9b728fc9c8c127c68d074b69b8c
Masiu <ciszekmarcell@gmail.com>
2020-10-09 19:29:01 +0200

    simple tests

--- transduce/index.ts
+++ transduce/index.ts
@@ -24,3 +24,28 @@ let list = [2, 5, 100, 23, 45, 24, 17, 19, 3, 4, 5, 6]
 let increase = mapWithReduce(list, addOne)
 let oddList = filterWithReduce(list, odd)
 // console.log(increase, oddList)
+
+const mapWithReduce2 = <T>(arr: T[]) => (mappingFn: Function) => {
+  return arr.reduce((list: T[], v: T) => {
+    return listCombination(list, mappingFn(v))
+  }, [])
+}
+
+const filterWithReduce2 = <T>(arr: T[]) => (predicateFn: Function) => {
+  return arr.reduce((list: T[], v) => {
+    return predicateFn(v) ? listCombination(list, v) : list
+  }, [])
+}
+
+let increase2 = mapWithReduce2(list)(addOne)
+let oddList2 = filterWithReduce2(list)(odd)
+
+export {
+  listCombination,
+  mapWithReduce,
+  filterWithReduce,
+  mapWithReduce2,
+  filterWithReduce2,
+  addOne,
+  odd,
+}
