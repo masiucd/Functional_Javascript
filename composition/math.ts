16a505acde09adf1e03754bd62bc322b06d06cfe
Masiu <ciszekmarcell@gmail.com>
2020-10-10 17:26:04 +0200

    add

--- composition/math.ts
+++ composition/math.ts
@@ -1,16 +1,16 @@
-type MathFnType = (x: number) => number;
-type FunctionTypes = MathFnType;
+type MathFnType = (x: number) => number
+type FunctionTypes = MathFnType
 
-export const minus2 = (x: number) => x - 2;
-export const triple = (x: number) => x * 3;
-export const addByOne = (x: number) => x + 1;
+export const minus2 = (x: number) => x - 2
+export const triple = (x: number) => x * 3
+export const addByOne = (x: number) => x + 1
 
 export const compoese = (...fns: FunctionTypes[]) => <T>(val: T) =>
   fns.reduce(
     (currentValue: T, currentFunction: Function) =>
       currentFunction(currentValue),
     val,
-  );
+  )
 
-let composeWithoutFn = addByOne(triple(minus2(10)));
-let composingMath = compoese(minus2, triple, addByOne)<number>(10);
+let composeWithoutFn = addByOne(triple(minus2(10)))
+let composingMath = compoese(minus2, triple, addByOne)<number>(10)
--- shapes/index.ts
+++ shapes/index.ts
@@ -1,19 +1,35 @@
 const not = (fn: Function) => {
-  return (...params: String[] | number[]) => !fn(...params);
-};
+  return (...params: String[] | number[]) => !fn(...params)
+}
 
 const isOdd = (n: number): boolean => {
-  return n % 2 === 1;
-};
+  return n % 2 === 1
+}
 
 const isEven = (n: number): boolean => {
-  return !isOdd(n);
-};
+  return !isOdd(n)
+}
 
-const isEvenNumber = not(isOdd);
-const isOddNumber = not(isEven);
+const isEvenNumber = not(isOdd)
+const isOddNumber = not(isEven)
 
 // console.log(isEvenNumber(6)); // true
 // console.log(isOdd(3)); // true
 
-export { not, isOdd, isEven };
+const dog = {
+  name: "bob",
+  breed: "bug",
+}
+
+const transformedMap = (mapFn: Function) => (map: Record<string, any>) => {
+  const copyMap = { ...map }
+  for (let key of Object.keys(map)) {
+    copyMap[key] = mapFn(copyMap[key])
+  }
+  return copyMap
+}
+
+let result = transformedMap((s: string) => s.toUpperCase())(dog)
+console.log(result)
+
+export { not, isOdd, isEven }
