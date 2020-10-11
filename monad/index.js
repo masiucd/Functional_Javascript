5aa0a4465db4dedb731da2bdf8fae9a1ddc33a37
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2020-10-11 12:58:32 +0200

    monads

--- monad/index.js
+++ monad/index.js
@@ -1,14 +1,25 @@
-const just = val => ({ map, chain, ap })
+const Just = val => ({ map, chain, ap })
 
-const map = fn => val => just(fn(val))
+const map = fn => val => Just(fn(val))
 
 // aka flatmap
 const chain = fn => val => fn(val)
 
 const ap = anotherMonad => val => anotherMonad.map(val)
 
-let six = just(6)
-let seven = x.map(x => x + 1)
+let six = Just(6)
+let seven = six.map(v => v + 1)
 
-console.log(six)
-console.log(seven)
+const identity = x => x
+
+let res = seven().chain(identity)
+
+let user1 = Just("user1")
+let user2 = Just("user2")
+
+const tuple = x => y => [x, y]
+
+const users = user1.map(tuple)().ap(user2)
+
+const Nothing = () => ({ map: Nothing, chain: Nothing, ap: Nothing })
+const Maybe = { Just, Nothing, of: Just }
